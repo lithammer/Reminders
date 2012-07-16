f5be5befac16c5b2ae882890c5b61b9901a16b0f
Peter Renström <renstrom.peter@gmail.com>
2012-07-16 16:56:41 +0200

    Revert from HasAnyAura() UnitAura()

--- modules/warlock.lua
+++ modules/warlock.lua
@@ -8,6 +8,6 @@ if config.enabled and addon.playerClass == "WARLOCK" then
 	end
 
 	addon:AddReminder("Missing Dark Intent", function() return not addon:HasSpellPowerBuff() end, {type = "spell", spell = "Dark Intent"})
-	addon:AddReminder("Missing Soul Link", function() return not addon:HasAnyAura("Soul Link") and hasDemon() and addon:HasTalent(7) end, {type = "spell", spell = "Soul Link"})
+	addon:AddReminder("Missing Soul Link", function() return not UnitAura("player", "Soul Link") and hasDemon() and addon:HasTalent(7) end, {type = "spell", spell = "Soul Link"})
 end
 
