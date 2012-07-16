f9075da4c7d6b790b942d10c10be637c5218de0f
Peter Renström <renstrom.peter@gmail.com>
2012-07-16 16:56:03 +0200

    Use new buff functions for Druid and Priest

--- modules/priest.lua
+++ modules/priest.lua
@@ -2,6 +2,6 @@ local addonName, addon = ...
 local config = addon.config.priest
 
 if config.enabled and addon.playerClass == "PRIEST" then
-	addon:AddReminder("Missing Power Word: Fortitude", function() return not addon:PlayerHasAnyAura({"Power Word: Fortitude", "Blood Pact"}) end, {type = "spell", unit = "player", spell = "Power Word: Fortitude"})
+	addon:AddReminder("Missing Power Word: Fortitude", function() return not addon:HasStaminaBuff() end, {type = "spell", unit = "player", spell = "Power Word: Fortitude"})
 	addon:AddReminder("Missing Inner Fire", function() return not addon:PlayerHasBuff("Inner Fire") end, {type = "spell", spell = "Inner Fire"})
 end
