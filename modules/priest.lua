ba9f391acbf0930c5f7fbeab44f0514771df0b6d
Peter Renström <renstrom.peter@gmail.com>
2012-07-16 15:56:55 +0200

    Vampiric Embrace is now a cooldown

--- modules/priest.lua
+++ modules/priest.lua
@@ -4,9 +4,4 @@ local config = addon.config.priest
 if config.enabled and addon.playerClass == "PRIEST" then
 	addon:AddReminder("Missing Power Word: Fortitude", function() return not addon:PlayerHasAnyAura({"Power Word: Fortitude", "Blood Pact"}) end, {type = "spell", unit = "player", spell = "Power Word: Fortitude"})
 	addon:AddReminder("Missing Inner Fire", function() return not addon:PlayerHasBuff("Inner Fire") end, {type = "spell", spell = "Inner Fire"})
-	
-	-- Shadow
-	if addon:HasTalentRank(3, 12) then
-		addon:AddReminder("Missing Vampiric Embrace", function() return not addon:PlayerHasBuff("Vampiric Embrace") end, {type = "spell", spell = "Vampiric Embrace"})
-	end
 end
