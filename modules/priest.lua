7dcbefbe2a9775b7786cb05e1211cbe108a99b8b
Peter Renström <renstrom.peter@gmail.com>
2012-07-19 11:11:03 +0200

    Priest: Add support for Inner Will.

--- modules/priest.lua
+++ modules/priest.lua
@@ -3,5 +3,5 @@ local config = addon.config.priest
 
 if config.enabled and addon.playerClass == "PRIEST" then
 	addon:AddReminder("Missing Power Word: Fortitude", function() return not addon:HasStaminaBuff() end, {type = "spell", unit = "player", spell = "Power Word: Fortitude"})
-	addon:AddReminder("Missing Inner Fire", function() return not addon:PlayerHasBuff("Inner Fire") end, {type = "spell", spell = "Inner Fire"})
+	addon:AddReminder("Missing Inner Fire/Will", function() return not addon:HasAnyAura({"Inner Fire", "Inner Will"}) end, {type = "spell", spell1 = "Inner Fire", spell2 = "Inner Will"})
 end
