eb1793d9feded1f038769a9bafd3d98ed1a20f16
Peter Renström <renstrom.peter@gmail.com>
2012-07-19 11:10:36 +0200

    Paladin: Remove Auras and Seals. And improve Blessing detection.

--- modules/paladin.lua
+++ modules/paladin.lua
@@ -2,13 +2,55 @@ local addonName, addon = ...
 local config = addon.config.paladin
 
 if config.enabled and addon.playerClass == "PALADIN" then
-	addon:AddReminder("No active aura", function() return not addon:HasAnyAura(config.auras, "PLAYER") end, {type = "spell", spell1 = config.auras[1], spell2 = config.auras[2]})
-	addon:AddReminder("No active seal", function() return not addon:HasAnyAura(config.seals, "PLAYER") end, {type = "spell", spell1 = config.seals[1], spell2 = config.seals[2]})
-	addon:AddReminder("Missing blessing", function() return not addon:HasAnyAura(config.blessings, "PLAYER") end, {type = "spell", unit = "player", spell1 = config.blessings[1], spell2 = config.blessings[2]})
-	
-	if config.righteousFury and (addon:HasTalentRank(2, 26) or UnitLevel("player") < 80) then
-		addon:AddReminder("Missing Righteous Fury", function() return addon:InPVEInstance() and not UnitAura("player", "Righteous Fury") end, {type = "spell", spell = "Righteous Fury"})
+	local CanBless = function()
+		if addon:HasStatsBuff() and addon:HasMasteryBuff() then
+			return false
+		end
+
+		local kings = select(8, UnitAura("player", "Blessing of Kings"))
+		local might = select(8, UnitAura("player", "Blessing of Might"))
+		if kings == "player" or might == "player" then
+			return false
+		end
+
+		return true
+	end
+
+	local CanBlessMight = function()
+		if CanBless() or addon:HasMasteryBuff() then
+			return false
+		end
+
+		local kings = select(8, UnitAura("player", "Blessing of Kings"))
+		if kings == "player" then
+			return false
+		end
+
+		return true
+	end
+
+	local CanBlessKings = function()
+		if CanBless() or addon:HasStatsBuff() then
+			return false
+		end
+
+		local might = select(8, UnitAura("player", "Blessing of Might"))
+		if might == "player" then
+			return false
+		end
+
+		return true
+	end
+
+	addon:AddReminder("Missing blessing", function() return CanBless() end, {type = "spell", unit = "player", spell1 = config.blessings[1], spell2 = config.blessings[2]})
+	addon:AddReminder("Missing Blessing of Might", function() return CanBlessMight() end, {type = "spell", unit = "player", spell = "Blessing of Might"})
+	addon:AddReminder("Missing Blessing of Kings", function() return CanBlessKings() end, {type = "spell", unit = "player", spell = "Blessing of Kings"})
+
+	local IsProtection = function()
+		return GetSpecialization() == 2
 	end
 	
-	addon:AddReminder("Crusader Aura active", function() return not IsMounted() and UnitAura("player", "Crusader Aura", nil, "PLAYER") end, {type = "spell", spell1 = config.auras[1], spell2 = config.auras[2]}, "spell_holy_crusaderaura")
+	if config.righteousFury then
+		addon:AddReminder("Missing Righteous Fury", function() return addon:InPVEInstance() and IsProtection() and not UnitAura("player", "Righteous Fury") end, {type = "spell", spell = "Righteous Fury"})
+	end
 end
