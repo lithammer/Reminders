bd89aad6b295b0160c3ceb03b2248d4f7aa38e80
Peter Renström <renstrom.peter@gmail.com>
2012-07-19 11:08:55 +0200

    Hunter: Use hunter config instead of warlock. And remove talent rank check.

--- modules/hunter.lua
+++ modules/hunter.lua
@@ -1,9 +1,9 @@
 local addonName, addon = ...
-local config = addon.config.warlock
+local config = addon.config.hunter
 
 if config.enabled and addon.playerClass == "HUNTER" then
 	-- Improved tracking
-	if addon:HasTalentRank(tabIndex, talentIndex) then
+	if UnitLevel("player") >= 80 then
 		addon:AddReminder("Undead tracking inactive within Icecrown Citadel", function() return GetTrackingTexture() ~= "Interface\\Icons\\Spell_Shadow_DarkSummoning" and GetZoneText() == "Icecrown Citadel" end, {type = "spell", spell = "Track Undead"})
 		addon:AddReminder("Dragonkin tracking inactive within Ruby Sanctum", function() return GetTrackingTexture() ~= "Interface\\Icons\\Spell_Shadow_DarkSummoning" and GetZoneText() == "The Ruby Sanctum" end, {type = "spell", spell = "Track Dragonkin"})
 	end
