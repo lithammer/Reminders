8bc49542a9f256b2d9e7205b08979dc6febdb9ef
renstrom <renstrom.peter@gmail.com>
2012-08-04 23:53:14 +0200

    Monk: LotWT got changed to a Crit buff.

--- modules/monk.lua
+++ modules/monk.lua
@@ -7,5 +7,5 @@ if config.enabled and addon.playerClass == "MONK" then
 	end
 
 	addon:AddReminder("Missing Legacy of the Emperor", function() return addon:MinPlayerLevel(22) and not addon:HasStatsBuff() end, {type = "spell", spell = "Legacy of the Emperor"})
-	addon:AddReminder("Missing Legacy of the White Tiger", function() return addon:MinPlayerLevel(81) and IsWindwalker() and not addon:HasMasteryBuff() end, {type = "spell", spell = "Legacy of the White Tiger"})
+	addon:AddReminder("Missing Legacy of the White Tiger", function() return addon:MinPlayerLevel(81) and IsWindwalker() and not addon:HasCriticalStrikeBuff() end, {type = "spell", spell = "Legacy of the White Tiger"})
 end
