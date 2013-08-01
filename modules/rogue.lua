7ac2f559effb22923bc70ba6d8edbef101f21602
Peter Renström <renstrom.peter@gmail.com>
2013-08-01 13:49:59 +0200

    [rogue] Add thresholdTime attribute to poison reminders. #1.

--- modules/rogue.lua
+++ modules/rogue.lua
@@ -2,6 +2,6 @@ local addonName, addon = ...
 local config = addon.config.rogue
 
 if config.enabled and addon.playerClass == "ROGUE" then
-	addon:AddReminder("Main hand poison", function() return not addon:HasAnyAura(config.mainHandPoisons) end, {type = "spell", spell1 = config.mainHandPoisons[1], spell2 = config.mainHandPoisons[2]})
-	addon:AddReminder("Off hand poison", function() return not addon:HasAnyAura(config.offHandPoisons) end, {type = "spell", spell1 = config.offHandPoisons[1], spell2 = config.offHandPoisons[2]})
+	addon:AddReminder("Main hand poison", function() return not addon:HasAnyAura(config.mainHandPoisons) end, {type = "spell", spell1 = config.mainHandPoisons[1], spell2 = config.mainHandPoisons[2], threshold = config.thresholdTime})
+	addon:AddReminder("Off hand poison", function() return not addon:HasAnyAura(config.offHandPoisons) end, {type = "spell", spell1 = config.offHandPoisons[1], spell2 = config.offHandPoisons[2], threshold = config.thresholdTime})
 end
