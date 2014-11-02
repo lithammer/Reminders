d62c936d3ffda8f9325767393aeaa1a679f40502
Peter Renström <renstrom.peter@gmail.com>
2014-11-02 15:18:24 +0100

    [mage] (Conjure) Mana Gem has been removed.

--- modules/mage.lua
+++ modules/mage.lua
@@ -2,30 +2,6 @@ local addonName, addon = ...
 local config = addon.config.mage
 
 if config.enabled and addon.playerClass == "MAGE" then	
-	local manaGems = {
-		["Rank 1"] = "Mana Agate",
-		["Rank 2"] = "Mana Jade",
-		["Rank 3"] = "Mana Citrine",
-		["Rank 4"] = "Mana Ruby",
-		["Rank 5"] = "Mana Emerald",
-		["Rank 6"] = "Mana Sapphire",
-	}
-	
-	local missingManaGem = function(self)
-		local name, rank, icon = GetSpellInfo("Conjure Mana Gem")
-		
-		if name then
-			local gem = manaGems[rank]
-		
-			if gem and GetItemCount(gem, false, true) == 3 then
-				self.setIcon(icon)
-					
-				return true
-			end
-		end
-	end
-	
-	addon:AddReminder("Less than 3 Mana Gems remaining", function(self) return missingManaGem(self) end, {type = "spell", spell = "Conjure Mana Gem"})
 	if GetSpellBookItemInfo("Dalaran Brilliance") then
 		addon:AddReminder("Missing Dalaran/Arcane Brilliance", function() return not addon:HasSpellPowerBuff() end, {type = "spell", spell1 = "Dalaran Brilliance", spell2 = "Arcane Brilliance"})
 	else
