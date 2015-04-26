8b41b922f96b174cd5efc32352015fd9159edaf5
Peter Renström <renstrom.peter@gmail.com>
2015-04-26 14:25:04 +0200

    [priest] Remove Inner Fire/Inner Will
    These spells are no longer in the game.

--- modules/priest.lua
+++ modules/priest.lua
@@ -3,5 +3,4 @@ local config = addon.config.priest
 
 if config.enabled and addon.playerClass == "PRIEST" then
 	addon:AddReminder("Missing Power Word: Fortitude", function() return not addon:HasStaminaBuff() end, {type = "spell", unit = "player", spell = "Power Word: Fortitude"})
-	addon:AddReminder("Missing Inner Fire/Will", function() return not addon:HasAnyAura(config.inners) end, {type = "spell", spell1 = config.inners[1], spell2 = config.inners[2]})
 end
